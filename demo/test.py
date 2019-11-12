ac34f9a6c14c9eee72f6be5059f77d379b5ee849
NoelleTemple <tug48873@temple.edu>
2019-11-12 18:25:59 -0500

    fix percentage error

--- demo/test.py
+++ demo/test.py
@@ -1,5 +1,8 @@
 from classification_pkg.classification import deepNet
+import sys
 
-obj = deepNet(img = "gr.jpg")
+#maybe this works? maybe it doesn't?
+image = sys.argv[1]
+obj = deepNet(img = image)
 obj.eval()
 print ("Object is {}, {} % sure".format(obj.getName(), obj.getPercent()))
